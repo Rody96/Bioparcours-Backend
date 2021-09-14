00986c217198f0d1c867fae46d43046f97e04996
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2021-09-14 23:38:22 +0200

    mongodb

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
+require('dotenv').config();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
 const port = 3000              
 
-app.get('/', (req, res) => { 
-    res.sendFile('index.html', {root: __dirname});
-                                                       
+app.listen(port, () => {
+    MongoClient.connect(process.env.CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(process.env.DB_NAME);
+        collection = database.collection(process.env.COLLECTION);
+        console.log("Connected to `" + process.env.DB_NAME + "`!");
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Now listening on port ${port}`); 
+app.get("/map", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/map/:id", (request, response) => {
+    collection.findOne({ "_id": request.params.id }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get('/', (req, res) => { 
+    res.sendFile('index.html', {root: __dirname});                                                
 });
